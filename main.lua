d0sboots/perfect-tower

commit dcd77dda4a2fe3bf0b3cfb71e68307e5a962bb44

Implement workspace moving
Also filter out empty scripts from workspace export, so that libraries
can be included in a workspace without it being super annoying.

main.lua
@@ -46,7 +46,9 @@ do
           assert = assert_old
           return false, pair.name .. "\n" .. ret
         end
-        exported[i] = ret
+        if ret ~= "" then
+          exported[#exported + 1] = ret
+        end
       end
       assert = assert_old
       return true, table.concat(exported, ";")
@@ -196,7 +198,7 @@ end
 
 -- importFunc takes a string (filename) and returns a pair of (status, string)
 -- (the content of the imported file on success, an error message on failure).
-function compile(name, input, importFunc, testing)
+function compile(name, input, importFunc, isExport)
   local variables, impulses, conditions, actions = {}, {}, {}, {}
   local env = clone_global()
   local macros, imported = {len = '__builtin__', lua = '__builtin__'}, {}
@@ -435,9 +437,12 @@ function compile(name, input, importFunc, testing)
   end
   package_name = package_name and package_name:sub(1, 24) .. ":" or ""
   script_name = script_name:sub(1, 24)
-  return testing and ret or string.format("%s\n%s %s %s\n%s",
-    package_name .. script_name, #impulses, #conditions, #actions, ret
-  )
+  if isExport then
+    return #impulses == 0 and #conditions == 0 and #actions == 0 and "" or ret
+  else
+    return string.format("%s\n%s %s %s\n%s",
+      package_name .. script_name, #impulses, #conditions, #actions, ret)
+  end
 end
 
 function import(input)

index.html
@@ -54,7 +54,7 @@
                     <button class="basicBtn" onclick="workspaceRename()">Rename Workspace</button>
                     <button class="basicBtn basicBtnRed" onclick="workspaceDelete()">Delete Workspace</button>
                     <button class="basicBtn" onclick="workspaceExport()">Export Workspace</button>
-                    <button class="basicBtn" onclick="workspaceMoveScript()">Move Script</button>
+                    <button id="move-script" class="basicBtn" onclick="workspaceMoveScript()">Move Script</button>
                 </div>
             </div>
         </div>
@@ -142,6 +142,7 @@
         }
 
         function scriptSelect(node) {
+            workspaceCancelMove();
             scriptSave();
             output.copy = undefined;
 

scripts/workspaces.js
@@ -2,8 +2,10 @@
 // This code treats 0 and 1 as reserved = All and Default
 let workspaces = ["All", "Default Workspace"];
 let currentWorkspace = workspaces[1];
+let movingScript = false;
 
 const workspaceList = document.getElementById("workspaceList");
+const moveButton = document.getElementById("move-script");
 
 function workspaceLoad() {
     // Load saved workspaces that do not exist yet
@@ -118,7 +120,15 @@ function workspaceChange(value) {
     workspaceList.value = value;
 
     // if "All" leave currentWorkspace as is but show all scripts
-    currentWorkspace = (value === workspaces[0] ? currentWorkspace : value);
+    // Otherwise, do move-script processing
+    if (value !== workspaces[0]) {
+      currentWorkspace = value;
+      if (movingScript) {
+        scripts[movingScript.id][2] = value;
+        scriptSave();
+        workspaceCancelMove();
+      }
+    }
 
     const scriptTabs = document.getElementById("scripts-tab").getElementsByTagName("LI");
 
@@ -160,7 +170,7 @@ function workspaceExportDone(result) {
     }
 
     if (result.value.length == 0) {
-        output.value = "There are no scripts here";
+        output.value = "There are no scripts here, or they are all libraries (produce no code)";
         return;
     }
 
@@ -170,7 +180,15 @@ function workspaceExportDone(result) {
 
 function workspaceMoveScript() {
     if (!activeTab) return;
+    if (!movingScript) {
+      movingScript = activeTab;
+      moveButton.classList.add("basicBtnCyan");
+    } else {
+      workspaceCancelMove();
+    }
+}
 
-    scripts[activeTab.id][2] = currentWorkspace;
-    scriptSave();
+function workspaceCancelMove() {
+    moveButton.classList.remove("basicBtnCyan");
+    movingScript = false;
 }

style.css
@@ -30,6 +30,11 @@
 	border-color: red;
 }
 
+.basicBtnCyan{
+	background-color: #00bbbb;
+	border-color: #00bbbb;
+}
+
 .functionList{
 	display: inline;
 }

commit 3fbc5c96666985e625ea7bd28594ba013c1dceb4

Fix unittests
One of the unittests has been broken since
dcd77dda4a2fe3bf0b3cfb71e68307e5a962bb44. (Change to make empty scripts
produce no output). Whoops.

Fixes https://github.com/d0sboots/perfect-tower/issues/9

main.lua
@@ -646,7 +646,7 @@ function unittest()
     :local int acc
     :global string status
     #output status = "Number is: " . acc
-  ]], "CGltcG9ydGVkAAAAAAAAAAAAAAAA"},
+  ]], ""},
   import_test = {[[
     :import imported
     :import imported